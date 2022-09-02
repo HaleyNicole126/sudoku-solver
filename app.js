cb2a00f35aabf6f65d09f0d30cf6e71555b99037
haybales <haleynicole126@gmail.com>
2022-09-01 21:47:06 -0600

    backend

--- app.js
+++ app.js
@@ -1,80 +1,75 @@
-const puzzleBoard = document.querySelector('#puzzle')
-const solveButton = document.querySelector('#solve-button')
-const solutionDisplay = document.querySelector('#solution')
-const squares = 81
-const submission = []
+const puzzleBoard = document.querySelector("#puzzle");
+const solveButton = document.querySelector("#solve-button");
+const solutionDisplay = document.querySelector("#solution");
+const squares = 81;
+let submission = [];
 
 for (let i = 0; i < squares; i++) {
-    const inputElement = document.createElement('input')
-    inputElement.setAttribute('type', 'number')
-    inputElement.setAttribute('min', 1)
-    inputElement.setAttribute('max', 9)
+  const inputElement = document.createElement("input");
+  inputElement.setAttribute("type", "number");
+  inputElement.setAttribute("min", 1);
+  inputElement.setAttribute("max", 9);
 
-    if (
-        ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
-        ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
-        ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
-        ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
-        ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
-        ) {
-            inputElement.classList.add('odd-section')
-        }
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && i > 27 && i < 53) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)
+  ) {
+    inputElement.classList.add("odd-section");
+  }
 
-
-    puzzleBoard.appendChild(inputElement)
+  puzzleBoard.appendChild(inputElement);
 }
-
 
 const joinValues = () => {
-  const inputs = document.querySelectorAll('input')
-  inputs.forEach(input => {
-    if(input.value) {
-        submission.push(input.value)
+  const inputs = document.querySelectorAll("input");
+  inputs.forEach((input) => {
+    if (input.value) {
+      submission.push(input.value);
     } else {
-        submission.push('.')
+      submission.push(".");
     }
-  })
-  console.log(submission)
-}
+  });
+  console.log(submission);
+};
 
 const populateValues = (isSolvable, solution) => {
-    const inputs = document.querySelectorAll('input')
-    if(isSolvable && solution) {
-        inputs.forEach((input, i) => {
-            input.value = solution[i]
-        })
-        solutionDisplay.innerHTML = 'This is the answer'
-    } else {
-        solutionDisplay.innerHTML = 'This is not solvable'
-    }
-    
-}
-
-const solve = () => {
-    joinValues()
-    const data = submission.join('')
-    console.log('data', data)
-
-const options = {
-  method: 'POST',
-  url: 'https://solve-sudoku.p.rapidapi.com/',
-  headers: {
-    'content-type': 'application/json',
-    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
-    'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
-  },
-  data: {
-    puzzle: data
+  const inputs = document.querySelectorAll("input");
+  if (isSolvable && solution) {
+    inputs.forEach((input, i) => {
+      input.value = solution[i];
+    });
+    solutionDisplay.innerHTML = "This is the answer";
+  } else {
+    solutionDisplay.innerHTML = "This is not solvable";
   }
 };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-    populateValues(response.data.solvable, response.data.solution) 
-}).catch(function (error) {
-	console.error(error);
-});
+const solve = () => {
+  joinValues();
+  const data = { numbers: submission.join("")};
+  console.log("data", data);
 
-}
+  fetch("http://localhost:8000/solve", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+        populateValues(data.solvable, data.solution)
+        submission = []
+     console.log(data)
+    }
+)
+    .catch((error) => {
+      console.error("Error:", error);
+    })
+};
 
-solveButton.addEventListener('click', solve)
+solveButton.addEventListener("click", solve);
